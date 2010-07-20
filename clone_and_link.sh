8e817ac93973f0d6b2bbae2ec8257ddc9884d0a8
David Goodlad <david@goodlad.ca>
2010-07-20 21:38:07 +1000

    Fix the clone_and_link script

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd &&
-[ -d '.dot-files' ] || git clone git://github.com/dgoodlad/dot-files.git .dot-files &&
-ls -ld .dot-files/files/* .dot-files/files/.* | while read f; do
+[ -d '.dot-files' ] || git clone git://github.com/dgoodlad/dotfiles.git .dot-files &&
+ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
   [ "$f" == '.dot-files/files/.' ] ||
   [ "$f" == '.dot-files/files/..' ] ||
   [ "$f" == '.dot-files/files/.git' ] ||
