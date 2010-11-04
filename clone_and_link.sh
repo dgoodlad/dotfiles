5d5ac316fcbc8a5368aeb5fe9d4af7e3b6a9bb0a
David Goodlad <david@goodlad.ca>
2010-11-05 08:04:38 +1100

    Update clone-and-link to handle oh-my-zsh properly

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -2,9 +2,8 @@
 
 cd &&
 [ -d '.dot-files' ] || git clone git://github.com/dgoodlad/dotfiles.git .dot-files &&
-ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
-  [ "$f" == '.dot-files/files/.' ] ||
-  [ "$f" == '.dot-files/files/..' ] ||
+find .dot-files/files -depth 1 | while read f; do
   [ "$f" == '.dot-files/files/.git' ] ||
+  ([ "$f" == '.dot-files/oh-my-zsh' ] && ln -vsf "$f" .oh-my-zsh) ||
   ln -vsf "$f" .
 done
