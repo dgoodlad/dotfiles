3a430a3f4f062e69dc493066f5ba9cee38697ef5
David Goodlad <david@goodlad.net>
2016-05-28 22:44:59 +1000

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+abort() { echo "!!! $*" >&2; exit 1; }
+log()   { echo "--> $*"; }
+logn()  { printf -- "--> $* "; }
+logk()  { echo "OK"; }
+
+which stow 2>&1 >/dev/null || {
+    echo "Missing GNU stow; please install it then re-run $0"
+    echo "On OS X: brew install stow"
+    exit 1
+}
+
+pushd $(dirname $0)
+
+log "Installing OS-independent dotfiles:"
+stow emacs
+stow git
+stow mail
+stow mutt
+stow readline
+stow ruby
+stow tmux
+stow util
+stow vim
+stow zsh
+logk
+
+if [ ! -d $HOME/.emacs.d ]; then
+    log "Installing spacemacs:"
+    git clone https://github.com/syl20bnr/spacemacs $HOME/.emacs.d
+    logk
+fi
+
+if [ ! -d $HOME/.oh-my-zsh ]; then
+    log "Installing oh-my-zsh:"
+    git clone https://github.com/robbyrussell/oh-my-zsh $HOME/.oh-my-zsh
+    logk
+fi
+
+readonly os=$(uname -s)
+log "Installing dotfiles for ${os}"
+if [[ "${os}" == "Darwin" ]]; then
+    stow osx
+else
+    stow X11
+    stow awesome
+fi
+logk
